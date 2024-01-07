d082a12eb84d6053c145a18891e12aed88708ccc
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2024-01-07 14:11:04 -0400

    Added the performance functionality

--- server/index.js
+++ server/index.js
@@ -16,12 +16,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
-  dataOverallStat
+  dataOverallStat,
+  dataAffiliateStat
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -53,5 +55,6 @@ mongoose
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
